bda06321b4c2d3aea04e60b9209248eb22d9f6e1
ammaraboalrub3 <ammaryaser808@gmail.com>
2024-09-17 13:39:19 +0300

    change to the dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:note_application/view/note_body_view.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const NoteApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class NoteApp extends StatelessWidget {
+  const NoteApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(brightness: Brightness.dark),
+      debugShowCheckedModeBanner: false,
       routes: {
         NoteBodyView.id: (context) => const NoteBodyView(),
       },
