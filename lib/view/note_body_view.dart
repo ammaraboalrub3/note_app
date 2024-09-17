5f5c856b20ff52dcb632cc2cfd44b6141c5b80a0
ammaraboalrub3 <ammaryaser808@gmail.com>
2024-09-17 13:21:33 +0300

    first commit

---
+++ lib/view/note_body_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class NoteBodyView extends StatelessWidget {
+  const NoteBodyView({super.key});
+
+  static String id = "NoteBodyView";
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
