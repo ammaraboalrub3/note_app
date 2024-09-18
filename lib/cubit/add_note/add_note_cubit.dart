d57219771be1aa6ae37702a120aa9731c1104e6a
ammaraboalrub3 <ammaryaser808@gmail.com>
2024-09-18 14:16:11 +0300

    addNote Method inside AddNoteCubit with try-catch

--- lib/cubit/add_note/add_note_cubit.dart
+++ lib/cubit/add_note/add_note_cubit.dart
@@ -1,8 +1,22 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
+import 'package:note_application/constant.dart';
+import 'package:note_application/model/note_model.dart';
 
 part 'add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
+
+  addNote(NoteModel note) async {
+    emit(AddNoteLoading());
+    try {
+      var notesBox = Hive.box<NoteModel>(noteBox);
+      await notesBox.add(note);
+      emit(AddNoteSuccess());
+    } catch (e) {
+      emit(AddNoteFaluir(e.toString()));
+    }
+  }
 }
